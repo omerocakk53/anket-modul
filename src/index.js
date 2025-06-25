6df374afe5f9b27e5917fe85b1439c0df4c5d739
omerocakk53 <ocakomer53@gmail.com>
2025-06-25 15:53:14 +0300

    V1

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as SurveyCreate } from './components/SurveyCreate';
+export { default as SurveyList } from './components/SurveyList';
+export { default as SurveyShare } from './components/SurveyShare';
