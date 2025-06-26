167683e7c8fc37b21a080fee1798f4978d189a47
omerocakk53 <ocakomer53@gmail.com>
2025-06-26 17:18:55 +0300

    V1

--- src/pages/AnketOynatıcısıWrapper.jsx
+++ src/pages/AnketOynatıcısıWrapper.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import AnketOynatıcısı from "../components/AnketOynatıcısı";
 
-export default function AnketOynatıcısıWrapper(fetchSurveyById, cevaplariKaydet) {
+export default function AnketOynatıcısıWrapper({fetchSurveyById, cevaplariKaydet}) {
   const { surveyId } = useParams();
   return <AnketOynatıcısı surveyId={surveyId} fetchSurveyById={fetchSurveyById} cevaplariKaydet={cevaplariKaydet} />;
 }
