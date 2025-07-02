12712d91b5d24b89b6c0869fa7d13fde1de6b152
omerocakk53 <ocakomer53@gmail.com>
2025-07-02 15:14:31 +0300

    2.0.15 packages is rename

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    prefix: 'tw-',
     content: [
-        './src/**/*.{js,jsx,ts,tsx}',
+        './src/**/*.{js,jsx,ts,tsx}', 
     ],
     theme: {
         extend: {
