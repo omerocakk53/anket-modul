89b1711cbf3c0bb13d1542e2a6dcbf3fa51e4af2
omerocakk53 <ocakomer53@gmail.com>
2025-07-02 15:23:31 +0300

    2.0.15 packages is rename

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,7 @@
 // postcss.config.js
-const tailwindcss = require("tailwindcss")
-const autoprefixer = require("autoprefixer")
-
 module.exports = {
-  plugins: [
-    tailwindcss('./tailwind.config.js'), // 🎯 Tailwind config'ini dahil et!
-    autoprefixer(),
-  ],
-}
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
