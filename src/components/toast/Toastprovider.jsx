0477225cd558978e3a80e0f8c2299129465c4402
omerocakk53 <ocakomer53@gmail.com>
2025-07-03 15:39:43 +0300

    Update 2.0.18

--- src/components/toast/Toastprovider.jsx
+++ src/components/toast/Toastprovider.jsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import { ToastContainer, toast, Slide, Zoom, Flip, Bounce } from 'react-toastify';
+import React, { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 const ToastProvider = ({ children }) => {
-    return (
-        <>
-            {children}
-            <ToastContainer
-                position="top-right"      // Toast'un çıkacağı yer
-                autoClose={3000}          // Otomatik kapanma süresi (ms)
-                hideProgressBar={false}   // İlerleme barı gösterilsin mi
-                newestOnTop={false}       // Yeni toast en üste mi
-                closeOnClick              // Tıklayınca kapanır
-                rtl={false}               // Sağdan sola dil için false
-                pauseOnFocusLoss          // Sekme değişince duraklat
-                draggable                // Sürüklenebilir
-                pauseOnHover             // Üzerine gelince duraklat
-                theme="light"            // light | dark | colored
-                transition={Slide}       // Slide, Zoom, Flip, Bounce
-            />
-        </>
-    );
+  return (
+    <>
+      {children}
+      <ToastContainer
+        position="top-left"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="colored"
+      />
+    </>
+  );
 };
 
 export default ToastProvider;
-
