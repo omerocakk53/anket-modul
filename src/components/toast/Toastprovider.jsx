c88fd7aa67566f3296090094257def19c687a606
omerocakk53 <ocakomer53@gmail.com>
2025-07-03 15:28:51 +0300

    Update 2.0.18

--- src/components/toast/Toastprovider.jsx
+++ src/components/toast/Toastprovider.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { ToastContainer, toast, Slide, Zoom, Flip, Bounce } from 'react-toastify';
 
-const ToastProvider = () => {
+const ToastProvider = ({ children }) => {
     return (
-        <ToastContainer
-            position="top-right"      // Toast'un çıkacağı yer
-            autoClose={3000}          // Otomatik kapanma süresi (ms)
-            hideProgressBar={false}   // İlerleme barı gösterilsin mi
-            newestOnTop={false}       // Yeni toast en üste mi
-            closeOnClick              // Tıklayınca kapanır
-            rtl={false}               // Sağdan sola dil için false
-            pauseOnFocusLoss          // Sekme değişince duraklat
-            draggable                // Sürüklenebilir
-            pauseOnHover             // Üzerine gelince duraklat
-            theme="light"            // light | dark | colored
-            transition={Slide}       // Slide, Zoom, Flip, Bounce
-        />
+        <>
+            {children}
+            <ToastContainer
+                position="top-right"      // Toast'un çıkacağı yer
+                autoClose={3000}          // Otomatik kapanma süresi (ms)
+                hideProgressBar={false}   // İlerleme barı gösterilsin mi
+                newestOnTop={false}       // Yeni toast en üste mi
+                closeOnClick              // Tıklayınca kapanır
+                rtl={false}               // Sağdan sola dil için false
+                pauseOnFocusLoss          // Sekme değişince duraklat
+                draggable                // Sürüklenebilir
+                pauseOnHover             // Üzerine gelince duraklat
+                theme="light"            // light | dark | colored
+                transition={Slide}       // Slide, Zoom, Flip, Bounce
+            />
+        </>
     );
 };
 
 export default ToastProvider;
+
