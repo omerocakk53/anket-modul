26765d0ba005249c2933cbe238dffaeda4db596b
omerocakk53 <ocakomer53@gmail.com>
2025-09-06 17:33:08 +0300

    fix: import

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ export { default as Surveys } from './src/pages/Surveys';
 export { default as SurveyPlayerWrapper } from './src/pages/SurveyPlayerWrapper';
 export { default as SurveyList } from './src/pages/SurveyList';
 export { default as SurveyCreate } from './src/components/SurveyCreate/index';
-export { default as SuccessMessagePortal } from './src/components/SuccessMesage/SuccessMessagePortal';
+export { default as SuccessMessagePortal } from './src/components/successMesage/SuccessMessagePortal';
 export { default as AnswerSheet } from './src/components/answer/answerComponents/AnswerTableComponent/AnswerSheet';
 export { default as Share } from './src/components/Share/Share';
 export { default as Answer } from './src/components/answer/AnswerPage';
